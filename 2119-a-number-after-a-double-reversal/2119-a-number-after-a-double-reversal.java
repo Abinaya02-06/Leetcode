47e1ef9f955227f18ac6637f8b1e26586f89eb4a
Abinaya02-06 <abinayab.cse2023@dce.edu.in>
2026-06-29 13:52:43 +0530

    Time: 0 ms (100%), Space: 41.9 MB (83.03%) - LeetHub

---
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isSameAfterReversals(int n) {
+        int count=0;
+        int div=1;
+        if(n%10!=0||n==0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
