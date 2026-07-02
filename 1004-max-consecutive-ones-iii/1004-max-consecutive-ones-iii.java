e918c27cca9ecdafc0b00d203ea01f903134e165
Abinaya02-06 <abinayab.cse2023@dce.edu.in>
2026-07-02 14:50:53 +0530

    Time: 3 ms (96.26%), Space: 51.9 MB (87.82%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -10,7 +10,7 @@ class Solution
             if(nums[right]==0)
             {
                 count++;
-                while(left<=right && count>k)
+                while( count>k)
                 {
                      if(nums[left]==0)
                     {
