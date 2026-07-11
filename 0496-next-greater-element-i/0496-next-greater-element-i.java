70f8b430f3004a7bbf01873f8a6c9af74b417d4c
Abinaya02-06 <abinayab.cse2023@dce.edu.in>
2026-07-11 12:05:52 +0530

    Time: 5 ms (16.34%), Space: 45.6 MB (11.22%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -17,16 +17,17 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         var arr=nextGreaterElement(nums2);
         int i,j,index=0;
-        int[]ans=new int[nums1.length];
-        for(i=0;i<nums1.length;i++){
-            for(j=0;j<nums2.length;j++){
-               if(nums1[i]==nums2[j]){
-                int number=arr.get(j);
-                ans[index]=number;
-                index++;
-               } 
-            }
+        int []ans=new int[nums1.length];
+        HashMap<Integer,Integer>h1=new HashMap<>();
+        for(i=0;i<nums2.length;i++)
+        {
+            h1.put(nums2[i],i);
         }
-        return ans;
-    }
+        for(i=0;i<nums1.length;i++)
+        {
+            int index1=h1.get(nums1[i]);
+            ans[index++]=arr.get(index1);
+        }
+        return ans;   
+}
 }
